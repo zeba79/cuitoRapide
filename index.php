de59ac3757d46bdbfbd0d2a588f8319226e1c087
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-04-10 22:05:46 +0200

    Ajout du footer et d'une card

--- index.php
+++ index.php
@@ -12,9 +12,34 @@
       <ul class="nav nav-pills">
         <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Accueil</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Recettes</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
       </ul>
     </header>
   </div>
+<div class="container">
+  <div class="card" style="width: 18rem;">
+  <img src="./uploads/images/tareDeThonProvencal.jpg" class="card-img-top" alt="Tarte de Thon provençal">
+  <div class="card-body">
+    <h5 class="card-title">Flan de thon provençal</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Voir la recette</a>
+  </div>
+</div>
+</div>
+
+
+  <div class="container">
+  <footer class="py-3 mb-0 ">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
+  </footer>
+</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
